d808ce55a2d87c6ba7263c86b77e5f10571b0bac
Alvaro <alvarodeviveirosneto@gmail.com>
2018-02-20 11:33:32 -0300

    First Commit

---
+++ app/src/main/java/com/example/avro/helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.avro.helloworld;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtNome;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtNome = findViewById(R.id.edtNome);
+    }
+
+    public void salvar(View view) {
+        String nome = edtNome.getText().toString();
+        Toast.makeText(this, getString(R.string.nome_informado) + nome, Toast.LENGTH_SHORT).show();
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle(R.string.titulo_alert);
+        alert.setMessage(R.string.primeiroAppAndroid);
+        alert.setPositiveButton(R.string.ok, null);
+        alert.show();
+    }
+}
